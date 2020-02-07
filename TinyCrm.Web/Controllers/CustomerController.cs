7e218c1eac733d67cb06074c7d9292140fbcb98d
NikitasX <nikitasxts@gmail.com>
2020-02-07 16:31:29 +0200

    - Web application implementation and product, customer table creation

---
+++ TinyCrm.Web/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Autofac;
+using Microsoft.AspNetCore.Mvc;
+using TinyCrm.Core;
+using TinyCrm.Core.Data;
+using TinyCrm.Core.Model;
+
+namespace TinyCrm.Web.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public TinyCrmDbContext Context_;
+        public IContainer Container_;
+
+        public CustomerController ()
+        {
+            Container_ = ServiceRegistrator.GetContainer();
+            Context_ = Container_.Resolve<TinyCrmDbContext>();
+        }
+
+        public IActionResult Index()
+        {
+
+            var customerList = Context_.Set<Customer>().Take(100).ToList();
+
+            return View(customerList);
+        }
+    }
+}
---
+++ TinyCrm.Web/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Autofac;
+using Microsoft.AspNetCore.Mvc;
+using TinyCrm.Core;
+using TinyCrm.Core.Data;
+using TinyCrm.Core.Model;
+
+namespace TinyCrm.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        public TinyCrmDbContext Context_;
+        public IContainer Container_;
+
+        public ProductController()
+        {
+            Container_ = ServiceRegistrator.GetContainer();
+            Context_ = Container_.Resolve<TinyCrmDbContext>();
+        }
+
+        public IActionResult Index()
+        {
+            var productList = Context_.Set<Product>().Take(100).ToList();
+
+            return View(productList);
+        }
+    }
+}
